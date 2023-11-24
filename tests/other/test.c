b759aee5e6379e37a1005a472adfc159fd85dfa2
limbo cingo <mrcingosup@gmail.com>
2023-11-24 13:44:20 +0000

    making tests in C

--- tests/other/test.c
+++ tests/other/test.c
@@ -1,6 +1,43 @@
-#include "stdio.h"
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("test\n");
+char ** readf(char *filepath, char* lines[]) {
+    FILE * fp;
+
+    char * line = NULL;
+    size_t len  = 0;
+    
+    ssize_t line_len;
+
+    fp = fopen(filepath, "r");
+
+    if (fp == NULL) {
+        printf("ERROR:%s: file not found.\n", filepath);
+        exit(1);
+    }
+
+    int i = 0;
+    while ((line_len = getline(&line, &len, fp)) != -1) {
+        printf("%s", lines[i]);
+        lines[i] = line;
+        i++;
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+    
+    return lines;
+}
+
+int main(void)
+{
+    char * path    = "test.flood";
+    char * lines[];
+    readf(path, lines);
+    // int  size     = sizeof(lines) / sizeof(lines[0]);
+    printf("%s\n", lines[0]);
+
     return 0;
 }
